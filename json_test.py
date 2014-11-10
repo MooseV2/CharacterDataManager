ffd3fb1f32a13e244637872811d5225004c7fdec
moose <haloshg@gmail.com>
2014-11-10 23:18:13 +1300

    Added files

---
+++ json_test.py
@@ -0,0 +1,38 @@
+#Add header
+import json
+import Tkinter as tk
+
+root = tk.Tk()
+
+with open('data.json') as json_data:
+    data = json.load(json_data)
+    json_data.close()
+
+print data
+
+
+'''
+for faction in data:
+    print "Faction: ", faction
+    for countries in data[faction]:
+        print "->\tCountry", countries
+        for units in data[faction][countries]:
+            print units
+            for unitinfo in data[faction][countries][units]:
+                print "->\t->\t", unitinfo, "\t:\t", data[faction][countries][units][unitinfo]'''
+
+
+def hello():
+    print "hello!"
+
+menubar = tk.Menu(root)
+
+# create a pulldown menu, and add it to the menu bar
+for faction in data:
+    menu = tk.Menu(menubar, tearoff=0)
+
+
+# display the menu
+root.config(menu=menubar)
+
+root.mainloop()
---
+++ widget_handler.py
@@ -0,0 +1,55 @@
+#Widgets file
+#Anthony DeSouza
+
+import Tkinter as tk
+
+class DataEntry(tk.Frame):
+    def __init__(self, master, label='', value=0, min_value=None, max_value=None):
+        #Attributes
+        self.value = value
+        self.name = label
+        self.min_value = min_value
+        self.max_value = max_value
+
+        tk.Frame.__init__(self, master)
+        self.master = master
+
+        self.name_var = tk.StringVar(self, value=self.name)
+        self.name_label = tk.Label(self, textvariable=self.name_var)
+        self.name_label.grid(row=0, columnspan=3)
+
+        self.button_plus = tk.Button(self, text="+", command=self.increase_value)
+        self.button_plus.grid(row=1, column=2)
+
+        self.button_minus = tk.Button(self, text="-", command=self.decrease_value)
+        self.button_minus.grid(row=1, column=0)
+
+        self.value_var = tk.IntVar(self, value=self.value)
+        self.value_var.trace("w", self.change_entry)
+        self.value_box = tk.Entry(self, textvariable=self.value_var, justify=tk.CENTER, width=4)
+        self.value_box.grid(row=1, column=1)
+
+    def change_entry(self, *args):
+        try:
+            text_value = self.value_var.get()
+            self.value = text_value
+        except ValueError:
+            pass
+
+    def change_value(self, new_value=0):
+        self.value = new_value
+        if (self.max_value != None):
+            self.value = min(self.value, self.max_value)
+        if (self.min_value != None):
+            self.value = max(self.value, self.min_value)
+        self.value_var.set(self.value)
+
+    def increase_value(self, amount=1):
+        self.change_value(self.value + amount)
+
+    def decrease_value(self, amount=1):
+        self.change_value(self.value - amount)
+
+    def set_label(self, label):
+        self.name = label
+        self.name_var.set(label)
